ede2c12599350b5fd622a13d9c0b23bcb8eb3820
Rob McCoy <rollcast@gmail.com>
2017-11-07 15:54:56 -0500

    First working api

--- readme.txt
+++ readme.txt
@@ -5,3 +5,14 @@ CMD: cd todo-api
 VSCODE: F1 - bash in workspace
 BASH: git init
 BASH: NPM init (enter to all and YES)
+VSCODE: create .gitignore
+VSCODE: node_modules/
+BASH: npm install express@4.13.3 --save
+VSCODE: +server.js
+VSCODE TERMINAL: node server.js to test to see if code is good.
+VSC TERM: heroku login
+VSC TERM: heroku create
+VSC TERM: heroku rename itrmc-todo-api
+VSC TERM: git push heroku master
+Chrome: https://itrmc-todo-api.herokuapp.com
+OR VSC TERM: heroku open
--- server.js
+++ server.js
@@ -1,11 +1,51 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000; //heroku uses the env
+var todos = [
+    {
+        id: 1,
+        description: 'Meet Justin for lunch',
+        completed: false
+    }, {
+        id: 2,
+        description: 'Go to market',
+        completed: false
+    }, {
+        id: 3,
+        description: 'Feed Isa',
+        completed: true
+    }
 
-app.get('/', function(req,res) {
+];
+
+app.get('/', function (req, res) {
     res.send('Todo API root');
 });
 
-app.listen(PORT, function(){
-    console.log('Express listening on PORT'+PORT);
+
+//GET /todos
+app.get('/todos', function (req,res) {
+    res.json(todos);
+});
+
+//GET /todos/:id
+app.get('/todos/:id', function (req,res) { //:id is an express notation. express knows to match that element and call it id
+    var todoID = req.params.id;
+    var response = null;
+    for (var i = 0; i< todos.length; i ++) {
+        if (todos[i].id == todoID) { response = todos[i]; break }
+    }
+    if (response) {
+        res.json(response);
+    } else {
+        res.status(404).send();
+    }
+    //
+    // res.send('Asking for todo with ID of '+req.params.id); //params is an express construct
+});
+
+
+
+app.listen(PORT, function () {
+    console.log('Express listening on PORT' + PORT);
 });
