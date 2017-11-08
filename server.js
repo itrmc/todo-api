a4bb02e11f4d59a2f2b35b6de42dbd7613dd4bb9
Rob McCoy <rollcast@gmail.com>
2017-11-08 11:27:17 -0500

    Added delete route

--- server.js
+++ server.js
@@ -31,42 +31,61 @@ app.get('/todos/:id', function (req, res) { //:id is an express notation. expres
             .send();
     }
     // res.send('Asking for todo with ID of '+req.params.id); //params is an express
-    
+
 });
 
+// DELETE /todos/:id GET /todos/:id
+app.delete('/todos/:id', function (req, res) { //:id is an express notation. express knows to match that element and call it id
+    var todoID = parseInt(req.params.id);
+
+    var response = _.findWhere(todos, {id: todoID}); //underscore library lets you maintain less code
+    if (response) {
+        todos = _.without(todos, response)
+        res.json(response);
+    } else {
+        res
+            .status(404)
+            .send();
+    }
+    // res.send('Asking for todo with ID of '+req.params.id); //params is an express
+
+});
 
 //POST /todos
-app.post('/todos', function (req,res){
-    try { var body = req.body }
-    catch (e) {
-        return res.status(400).send(e); //400 means bad data was provided
+app.post('/todos', function (req, res) {
+    try {
+        var body = req.body
+    } catch (e) {
+        return res
+            .status(400)
+            .send(e); //400 means bad data was provided
     }
 
-//    console.log('description: '+body.description);
-
-
-// eliminate malformed body
-/* 
+    //    console.log('description: '+body.description); eliminate malformed body
+    /*
 try {JSON.parse(body) }
 catch (e) {
     return res.status(400).send(e); //400 means bad data was provided
     }
 */
     // eliminate unexpected objects or elements
-    body = _.pick(body, 'description','completed');
-    
-// eliminate unexpected data types
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
-        return res.status(400).send(); //400 means bad data was provided
+    body = _.pick(body, 'description', 'completed');
+
+    // eliminate unexpected data types
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res
+            .status(400)
+            .send(); //400 means bad data was provided
     }
 
-    body.description = body.description.trim();
+    body.description = body
+        .description
+        .trim();
     body.id = todoNextID++;
     todos.push(body);
     res.json(body);
 
 });
-
 
 app.listen(PORT, function () {
     console.log('Express listening on PORT' + PORT);
